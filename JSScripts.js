a72bf76ff2209114f8c211eacb9f550d8a0b5116
Olya <Zlobinaolya87@gmail.com>
2022-09-16 09:08:09 +0300

    initial commit

---
+++ JSScripts.js
@@ -0,0 +1,18 @@
+
+
+// let p = Number.parseInt(prompt('Введите число'));
+// alert(`Результат: ${p+2}`);
+
+
+// let a = prompt('Введите имя',"Оля");
+// alert(`Привет ${a}!`);
+
+// const name = function(){
+// let a =  prompt('Введите имя')
+// return a;
+// }
+
+function showName(name){
+alert(`Привет ${name}`);
+}
+showName(Name);
